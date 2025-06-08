aafacb39a0536fc4641250432e17f6614e6a547e
Devender <devenderkumar0194@gmail.com>
2025-06-08 14:03:06 +0530

    update

--- FrontEnd/src/Component/Product/ProductAdd.jsx
+++ FrontEnd/src/Component/Product/ProductAdd.jsx
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import { useDispatch, useSelector } from 'react-redux';
 
-
+import { addProduct, clearAddError } from "../../app/features/product/productSlice";
 
 
 function ProductAdd() {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [success, setSuccess] = useState("");
+    const { addStatus, addError } = useSelector((state) => state.products);
 
+  const dispatch = useDispatch();
 
    const initialValues = {
         title : '',
@@ -32,18 +36,39 @@ function ProductAdd() {
         }),
         onSubmit: async (values) => {
 
-         
-            setOpen(false);
+            dispatch(addProduct( values ));
+            if(addStatus === 'succeeded'){
+                setSuccess("Product added successfully");
+                setOpen(false);
+                setSuccess("");
+
+                formik.resetForm();
+
+            }else{
+                setOpen(true);
+            }
 
         }
       });
+
+
+      const productModal = () => {
+        setOpen(true);
+        dispatch(clearAddError());
+      } 
+
+      const backResetForm = () => {
+        dispatch(clearAddError());
+        setOpen(false);
+        formik.resetForm();
+      }
 
 
   return (
     <>
         <div className="flex justify-end">
             <button
-                onClick={() => setOpen(true)}
+                onClick={productModal}
                 className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 type="button">
                     Add
@@ -57,7 +82,7 @@ function ProductAdd() {
                             <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                                 Add Product 
                             </h3>
-                            <button type="button" onClick={() => setOpen(false)} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                            <button type="button" onClick={backResetForm} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
                                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                                 </svg>
@@ -68,10 +93,11 @@ function ProductAdd() {
                         <form className="mx-auto text-left" onSubmit={formik.handleSubmit}>
 
                         <div className="p-4 md:p-5 space-y-4">
-
+                            <p className="text-red-600">{addError}</p>
+                            <p className="text-green-600">{success}</p>
                             <div className="mb-5" >
                                 <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                                <input type="text" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter title"
+                                <input autoComplete="off" type="text" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter title"
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
                                 value={formik.values.title}
@@ -83,7 +109,7 @@ function ProductAdd() {
 
                             <div className="mb-5" >
                                 <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-                                <input type="text" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter price"
+                                <input autoComplete="off" type="text" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter price"
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
                                 value={formik.values.price}
@@ -95,7 +121,7 @@ function ProductAdd() {
 
                             <div className="mb-5" >
                                 <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                                <textarea id="description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here..."
+                                <textarea autoComplete="off" id="description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here..."
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
                                 value={formik.values.description}
@@ -109,7 +135,7 @@ function ProductAdd() {
                         <div className="flex justify-end items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
                             
                             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
-                            <button onClick={() => setOpen(false)} className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Back</button>
+                            <button onClick={backResetForm} className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Back</button>
                         </div>
 
                         </form>
--- FrontEnd/src/app/features/product/productSlice.js
+++ FrontEnd/src/app/features/product/productSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const backEndURL = "http://localhost:3000";
 
@@ -13,14 +14,56 @@ export const fetchProducts = createAsyncThunk(
 );
 
 
+// Add product
+export const addProduct = createAsyncThunk('products/addProduct', async (newProduct, { rejectWithValue }) => {
+  try {
+      const response = await axios.post(backEndURL+'/api/add-product', newProduct);
+      return response.data; // success path
+  } catch (err) {
+    // Return 409 or any error message from API
+    if (err.response && err.response.data) {
+      return rejectWithValue(err.response.data);
+    }
+    return rejectWithValue({ message: err.message });
+  }
+
+
+  return response.data;
+});
+
+
+
+// // Edit product
+// export const editProduct = createAsyncThunk('products/editProduct', async (product) => {
+//   const response = await axios.put(`/api/products/${product.id}`, product);
+//   return response.data;
+// });
+
+// // Delete product
+// export const deleteProduct = createAsyncThunk('products/deleteProduct', async (id) => {
+//   await axios.delete(`/api/products/${id}`);
+//   return id;
+// });
+
+
 const productSlice = createSlice({
   name: 'products',
   initialState: {
     items: [],
     status: 'idle', // loading | succeeded | failed
     error: null,
+    addStatus: 'idle',
+    addError: null,
+
   },
-  reducers: {},
+  reducers: {
+      setAddError(state, action) {
+        state.addError = action.payload;
+      },
+      clearAddError(state) {
+        state.addError = null;
+      },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchProducts.pending, (state) => {
@@ -35,8 +78,40 @@ const productSlice = createSlice({
       .addCase(fetchProducts.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
-      });
+      })
+
+      // ADD
+      .addCase(addProduct.pending, (state) => {
+        state.addStatus = 'loading';
+      })
+      .addCase(addProduct.fulfilled, (state, action) => {
+          state.addStatus = 'succeeded';
+          state.items.unshift(action.payload.data);
+          state.addError = null;
+      })
+      .addCase(addProduct.rejected, (state, action) => {
+        state.addStatus = 'failed';
+        state.addError = action.payload.message;
+      })
+
+
+      // // EDIT
+      // .addCase(editProduct.fulfilled, (state, action) => {
+      //   const index = state.items.findIndex((p) => p.id === action.payload.id);
+      //   if (index !== -1) {
+      //     state.items[index] = action.payload;
+      //   }
+      // })
+
+      // // DELETE
+      // .addCase(deleteProduct.fulfilled, (state, action) => {
+      //   state.items = state.items.filter((p) => p.id !== action.payload);
+      // });
+
   },
 });
+
+export const { setAddError, clearAddError } = productSlice.actions;
+
 
 export default productSlice.reducer;
